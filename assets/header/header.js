726baa2b860467d920edec4ce203c06db16e6d24
Ian Vu <ianv6207@mytusd.org>
2026-04-20 20:23:57 -0700

    Create header.js

---
+++ assets/header/header.js
@@ -0,0 +1,6 @@
+const header = document.createElement("div");
+header.id = "header";
+const headerIcon = document.createElement("img");
+headerIcon.width = "20pt";
+headerIcon.height = "20pt";
+headerIcon.src = "https://ianCVu.github.io/ian.C.chat/assets/icons/header_icon.png";
